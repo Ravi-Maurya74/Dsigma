ff487fb160a94dacd097111205735fc73ae9ea66
Ravi <lcs2020043@iiitl.ac.in>
2023-07-20 00:22:20 +0530

    added animations

--- lib/pages/tab2.dart
+++ lib/pages/tab2.dart
@@ -13,6 +13,7 @@ class Tab2 extends StatelessWidget {
         children: [
           Stack(
             children: [
+              const AnimatedGamla(),
               Container(
                 height: 300,
                 width: 300,
@@ -22,7 +23,42 @@ class Tab2 extends StatelessWidget {
                   ),
                 ),
               ),
-              const Sandwich()
+              const AnimatedFoodLeft(
+                  path: "assets/food/sandwich.png",
+                  topStart: 100,
+                  topEnd: 130,
+                  leftStart: 70,
+                  leftEnd: 50),
+              const AnimatedFoodLeft(
+                  path: "assets/food/donut.png",
+                  topStart: 60,
+                  topEnd: 70,
+                  leftStart: 50,
+                  leftEnd: 10),
+              const AnimatedFoodLeft(
+                  path: "assets/food/burger.png",
+                  topStart: 15,
+                  topEnd: 0,
+                  leftStart: 80,
+                  leftEnd: 50),
+              const AnimatedFoodRight(
+                  path: "assets/food/salad.png",
+                  topStart: 15,
+                  topEnd: 0,
+                  rightStart: 80,
+                  rightEnd: 50),
+              const AnimatedFoodRight(
+                  path: "assets/food/aple.png",
+                  topStart: 60,
+                  topEnd: 70,
+                  rightStart: 50,
+                  rightEnd: 10),
+              const AnimatedFoodRight(
+                  path: "assets/food/fish.png",
+                  topStart: 100,
+                  topEnd: 130,
+                  rightStart: 70,
+                  rightEnd: 50),
             ],
           ),
           const SizedBox(
@@ -52,19 +88,73 @@ class Tab2 extends StatelessWidget {
   }
 }
 
-class Sandwich extends StatefulWidget {
-  const Sandwich({
+class AnimatedGamla extends StatefulWidget {
+  const AnimatedGamla({
     super.key,
   });
 
   @override
-  State<Sandwich> createState() => _SandwichState();
+  State<AnimatedGamla> createState() => _AnimatedGamlaState();
 }
 
-class _SandwichState extends State<Sandwich> {
-  bool finished = false;
-  void Animate() {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
+class _AnimatedGamlaState extends State<AnimatedGamla> {
+  late Timer _timer;
+  bool left = true;
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        left = !left;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+        duration: const Duration(milliseconds: 500),
+        bottom: 0,
+        curve: Curves.easeIn,
+        left: left ? 0 : 220,
+        child: const SizedBox(
+          height: 150,
+          width: 80,
+          child: Image(image: AssetImage("assets/images/gamla1.png")),
+        ));
+  }
+}
+
+class AnimatedFoodLeft extends StatefulWidget {
+  const AnimatedFoodLeft({
+    super.key,
+    required this.path,
+    required this.topStart,
+    required this.topEnd,
+    required this.leftStart,
+    required this.leftEnd,
+  });
+  final String path;
+  final double topStart, topEnd, leftStart, leftEnd;
+
+  @override
+  State<AnimatedFoodLeft> createState() => _AnimatedFoodLeftState();
+}
+
+class _AnimatedFoodLeftState extends State<AnimatedFoodLeft> {
+  bool finished = true;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         finished = !finished;
       });
@@ -72,22 +162,77 @@ class _SandwichState extends State<Sandwich> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    Animate();
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return AnimatedPositioned(
       duration: const Duration(milliseconds: 500),
-      top: finished ? 70 : 90,
-      left: finished ? 60 : 20,
-      child: const SizedBox(
+      curve: Curves.easeIn,
+      top: finished ? widget.topStart : widget.topEnd,
+      left: finished ? widget.leftStart : widget.leftEnd,
+      child: SizedBox(
         height: 40,
         width: 40,
         child: Image(
-          image: AssetImage("assets/food/sandwich.png"),
+          image: AssetImage(widget.path),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedFoodRight extends StatefulWidget {
+  const AnimatedFoodRight({
+    super.key,
+    required this.path,
+    required this.topStart,
+    required this.topEnd,
+    required this.rightStart,
+    required this.rightEnd,
+  });
+  final String path;
+  final double topStart, topEnd, rightStart, rightEnd;
+
+  @override
+  State<AnimatedFoodRight> createState() => _AnimatedFoodRightState();
+}
+
+class _AnimatedFoodRightState extends State<AnimatedFoodRight> {
+  bool finished = true;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        finished = !finished;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeIn,
+      top: finished ? widget.topStart : widget.topEnd,
+      right: finished ? widget.rightStart : widget.rightEnd,
+      child: SizedBox(
+        height: 40,
+        width: 40,
+        child: Image(
+          image: AssetImage(widget.path),
         ),
       ),
     );
--- lib/pages/walkthrough_page.dart
+++ lib/pages/walkthrough_page.dart
@@ -2,6 +2,7 @@ import 'package:dsigma/pages/tab1.dart';
 import 'package:dsigma/pages/tab2.dart';
 import 'package:dsigma/pages/tab3.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class WalkThroughScreen extends StatelessWidget {
@@ -66,21 +67,108 @@ class WalkThroughScreen extends StatelessWidget {
                       dotColor: Color(0xFFE0D4EC)),
                 ),
               ),
-              GestureDetector(
-                child: const CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Color(0xFF6318AF),
-                  child: Icon(
-                    Icons.arrow_forward,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+              BottomButton(controller: controller),
               const SizedBox(
-                height: 80,
+                height: 40,
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomButton extends StatefulWidget {
+  const BottomButton({
+    super.key,
+    required this.controller,
+  });
+
+  final PageController controller;
+
+  @override
+  State<BottomButton> createState() => _BottomButtonState();
+}
+
+class _BottomButtonState extends State<BottomButton> {
+  bool _expanded = false;
+  bool _text = false;
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_animate);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_animate);
+    super.dispose();
+  }
+
+  void _animate() {
+    if (widget.controller.page == 2) {
+      setState(() {
+        _expanded = true;
+        Future.delayed(
+          const Duration(seconds: 1),
+          () {
+            setState(() {
+              _text = true;
+            });
+          },
+        );
+      });
+    } else {
+      setState(() {
+        _text = false;
+        _expanded = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        HapticFeedback.vibrate();
+        widget.controller.nextPage(
+            duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFFE0D4EC),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            AnimatedContainer(
+              width: _expanded ? 140 : 0,
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeOut,
+              child: _text
+                  ? Center(
+                      child: Text(
+                      "Get Started",
+                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                          color: const Color(0xFF6318AF),
+                          fontWeight: FontWeight.bold),
+                    ))
+                  : null,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: CircleAvatar(
+                radius: 30,
+                backgroundColor: Color(0xFF6318AF),
+                child: Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
